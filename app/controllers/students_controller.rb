92eda4b96c019f316396f6076ce1d36c54d7b85c
+ <+>
2023-03-14 16:59:46 +0300

    completed work

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,21 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    render json: students, except: [:created_at, :updated_at]
+
   end
 
+  def show
+    students = Student.find(params[:id]) 
+    if students
+    # render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+      render json: students, except: [:created_at, :updated_at]
+      else
+        render json: { error: 'Student not found' }, status: :not_found
+      end
+  end
+    
 end
+
+
+
